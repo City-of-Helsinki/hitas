e008b22fc40bf7908540efbe2f3b949c6b856d21
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-06-07 18:41:52 +0300

    Add custom user model

---
+++ backend/users/models.py
@@ -0,0 +1,16 @@
+import uuid
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from helusers.models import AbstractUser
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} ({self.pk}): {self.get_username()}>"
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
