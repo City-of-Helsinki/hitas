efc4edf2eeff3a54e5d83912ce8f37163ff16f3f
Mats Eriksson (Vincit) <mats.eriksson@vincit.fi>
2022-06-09 21:04:07 +0300

    Create and config React CRA with typescript, eslint and prettier. Initial commit.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hitas</h1>
+	    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad deserunt dolorem eaque esse facilis fugiat fugit illo modi molestias neque nobis perspiciatis possimus ratione, recusandae, repudiandae saepe sapiente unde voluptatum.</p>
+    </div>
+  );
+}
+
+export default App;
