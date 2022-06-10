b18f16a73859fecdf23b5f5dbf6b7e0c98459270
Mats Eriksson (Vincit) <mats.eriksson@vincit.fi>
2022-06-10 18:04:30 +0300

    Helsinki Design System libraries added + component import tested. SASS & PostCSS support added.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,73 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+
+import { Navigation, Container } from "hds-react";
+
+import "./App.sass";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hitas</h1>
-	    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad deserunt dolorem eaque esse facilis fugiat fugit illo modi molestias neque nobis perspiciatis possimus ratione, recusandae, repudiandae saepe sapiente unde voluptatum.</p>
+      <Navigation
+        title="Hitas"
+        menuToggleAriaLabel=""
+        skipTo=""
+        skipToContentLabel=""
+      >
+        <Navigation.Row ariaLabel="Main navigation">
+          <Navigation.Item
+            href="#"
+            label="Link"
+            active
+            onClick={(e) => e.preventDefault()}
+          />
+          <Navigation.Item
+            href="#"
+            label="Link"
+            onClick={(e) => e.preventDefault()}
+          />
+          <Navigation.Item
+            href="#"
+            label="Link"
+            onClick={(e) => e.preventDefault()}
+          />
+          <Navigation.Item
+            href="#"
+            label="Link"
+            onClick={(e) => e.preventDefault()}
+          />
+          <Navigation.Dropdown label="Dropdown">
+            <Navigation.Item
+              href="#"
+              label="Link"
+              onClick={(e) => e.preventDefault()}
+            />
+            <Navigation.Item
+              href="#"
+              label="Link"
+              onClick={(e) => e.preventDefault()}
+            />
+            <Navigation.Item
+              href="#"
+              label="Link"
+              onClick={(e) => e.preventDefault()}
+            />
+            <Navigation.Item
+              href="#"
+              label="Link"
+              onClick={(e) => e.preventDefault()}
+            />
+          </Navigation.Dropdown>
+        </Navigation.Row>
+      </Navigation>
+      <Container className={"App"}>
+        <h1>Hitas</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad deserunt
+          dolorem eaque esse facilis fugiat fugit illo modi molestias neque
+          nobis perspiciatis possimus ratione, recusandae, repudiandae saepe
+          sapiente unde voluptatum.
+        </p>
+      </Container>
     </div>
   );
 }
