3cbfb8a2b62a7e8bb314b007bf4a68fe838a86dd
Mats Eriksson <mats.eriksson@vincit.fi>
2022-06-13 11:58:27 +0300

    Fixes linting with prettier

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders Hitas heading', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hitas/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders Hitas heading", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Hitas/i);
+    expect(linkElement).toBeInTheDocument();
 });
