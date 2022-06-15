d7b8ec59044a16f34836f460912e52917e174648
Tomi Oikarinen <tomi.oikarinen@vincit.fi>
2022-06-15 15:29:59 +0300

    Major docker changes & Makefile added
     - Docker changes
       - Dockerfile now uses builder for creating a virtualenv
       - Image contains /hitas/venv and hitas/backend directories
         - /hitas/venv is built by the builder
         - /hitas/backend contains only the necessary files for running the app
         - everything is owned by `hitas` user
       - Default image only now supports uwsgi. It exposes uwsgi port as 8888
       - docker-compose now starts nginx instance with port 8080 open. It
         will serve django app over uwsgi and static media files.
     - uwsgi support added (it was broken)
     - Added Makefile
       - contains a list of simple day-to-day commands
         - `make build` starts a docker-compose environment
         - `make tests` runs tests
         - `make format` runs linters
         - `make dump` takes a new initial datamodel dump

--- backend/format.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cmd='autoflake --recursive --in-place --remove-all-unused-imports --ignore-init-module-imports . && isort . && black . && flake8 .'
-
-if [ -f /.dockerenv ]; then
-    eval "$cmd"
-else
-    docker-compose run --no-deps --rm --entrypoint='/bin/sh -c' hitas "$cmd"
-fi
