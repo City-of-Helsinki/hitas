3de039541b372c8eb189d702b3d750e68160fc6a
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-06-30 03:32:23 +0300

    Refactor and abstract HousingCompanyViewSet

--- backend/hitas/views/housing_company.py
+++ backend/hitas/views/housing_company.py
@@ -1,19 +1,50 @@
 import datetime
 from collections import defaultdict
 from typing import Any, Dict, List, Optional
-from uuid import UUID
 
-from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Min
 from enumfields.drf.serializers import EnumSupportSerializerMixin
-from rest_framework import serializers, status, viewsets
-from rest_framework.response import Response
+from rest_framework import serializers
 
 from hitas.exceptions import HousingCompanyNotFound
 from hitas.models import Building, HousingCompany, PropertyManager, RealEstate
 from hitas.views.codes import BuildingTypeSerializer, DeveloperSerializer, FinancingMethodSerializer
-from hitas.views.helpers import Address, address_obj, value_or_none
-from hitas.views.paginator import get_default_paginator
+from hitas.views.helpers import Address, HitasModelViewSet, address_obj, value_or_none
+
+
+class BuildingSerializer(serializers.ModelSerializer):
+    address = serializers.SerializerMethodField()
+    building_identifier = serializers.SerializerMethodField()
+
+    def get_address(self, obj: Building) -> Address:
+        return address_obj(obj)
+
+    def get_building_identifier(self, obj: Building) -> Optional[str]:
+        return value_or_none(obj.building_identifier)
+
+    class Meta:
+        model = Building
+        fields = [
+            "address",
+            "building_identifier",
+            "completion_date",
+        ]
+
+
+class RealEstateSerializer(serializers.ModelSerializer):
+    address = serializers.SerializerMethodField()
+    buildings = BuildingSerializer(many=True)
+
+    def get_address(self, obj: RealEstate) -> Address:
+        return address_obj(obj)
+
+    class Meta:
+        model = RealEstate
+        fields = [
+            "address",
+            "property_identifier",
+            "buildings",
+        ]
 
 
 class HousingCompanyListSerializer(EnumSupportSerializerMixin, serializers.ModelSerializer):
@@ -180,144 +211,66 @@ class HousingCompanyDetailSerializer(EnumSupportSerializerMixin, serializers.Mod
         ]
 
 
-class RealEstateSerializer(serializers.ModelSerializer):
-    address = serializers.SerializerMethodField()
-    buildings = serializers.SerializerMethodField()
-
-    def get_address(self, obj: RealEstate) -> Address:
-        return address_obj(obj)
-
-    def get_buildings(self, obj: RealEstate) -> List[Building]:
-        return BuildingSerializer(self.context["buildings"][obj.id], many=True).data
-
-    class Meta:
-        model = RealEstate
-        fields = [
-            "address",
-            "property_identifier",
-            "buildings",
-        ]
-
-
-class BuildingSerializer(serializers.ModelSerializer):
-    address = serializers.SerializerMethodField()
-    building_identifier = serializers.SerializerMethodField()
-
-    def get_address(self, obj: Building) -> Address:
-        return address_obj(obj)
-
-    def get_building_identifier(self, obj: Building) -> Optional[str]:
-        return value_or_none(obj.building_identifier)
-
-    class Meta:
-        model = Building
-        fields = [
-            "address",
-            "building_identifier",
-            "completion_date",
-        ]
-
-
-class HousingCompanyView(viewsets.ModelViewSet):
-    queryset = HousingCompany.objects.all()
+class HousingCompanyViewSet(HitasModelViewSet):
     serializer_class = HousingCompanyDetailSerializer
-    permission_classes = []
-    lookup_field = "housing_company_id"
+    list_serializer_class = HousingCompanyListSerializer
+    not_found_exception_class = HousingCompanyNotFound
 
-    def list(self, request):
-        housing_companies = (
-            HousingCompany.objects.select_related("postal_code")
+    def get_list_queryset(self):
+        return (
+            HousingCompany.objects.select_related(
+                "postal_code",
+            )
             .annotate(date=Min("real_estates__buildings__completion_date"))
             .only("uuid", "state", "postal_code__value", "postal_code__description", "display_name", "street_address")
             .order_by("id")
         )
 
-        paginator = get_default_paginator()
-        result_page = paginator.paginate_queryset(housing_companies, request)
-        serializer = HousingCompanyListSerializer(result_page, many=True)
-
-        return Response(
-            {
-                "page": {
-                    "current_page": paginator.page.number,
-                    "size": len(result_page),
-                    "total_items": paginator.page.paginator.count,
-                    "total_pages": paginator.page.paginator.num_pages,
-                    "links": {
-                        "next": paginator.get_next_link(),
-                        "previous": paginator.get_previous_link(),
-                    },
-                },
-                "contents": serializer.data,
-            },
-            status=status.HTTP_200_OK,
-        )
-
-    def retrieve(self, request, housing_company_id=None):
-        try:
-            uuid = UUID(hex=housing_company_id)
-        except ValueError:
-            raise HousingCompanyNotFound()
-
-        try:
-            housing_company = (
-                HousingCompany.objects.extra(
-                    select={
-                        "date": """
-    SELECT MIN(completion_date) FROM hitas_building AS b
-        LEFT JOIN hitas_realestate AS re ON b.real_estate_id = re.id
-        WHERE re.housing_company_id = hitas_housingcompany.id
-    """
-                    }
-                )
-                .select_related(
-                    "postal_code",
-                    "financing_method",
-                    "developer",
-                    "building_type",
-                    "property_manager",
-                    "property_manager__postal_code",
-                    "last_modified_by",
-                )
-                .only(
-                    "uuid",
-                    "state",
-                    "postal_code__value",
-                    "postal_code__description",
-                    "financing_method__value",
-                    "financing_method__description",
-                    "financing_method__legacy_code_number",
-                    "developer__value",
-                    "developer__description",
-                    "developer__legacy_code_number",
-                    "building_type__value",
-                    "building_type__description",
-                    "building_type__legacy_code_number",
-                    "property_manager__name",
-                    "property_manager__email",
-                    "property_manager__street_address",
-                    "property_manager__postal_code__value",
-                    "property_manager__postal_code__description",
-                    "display_name",
-                    "street_address",
-                    "business_id",
-                    "official_name",
-                    "acquisition_price",
-                    "realized_acquisition_price",
-                    "primary_loan",
-                    "sales_price_catalogue_confirmation_date",
-                    "notification_date",
-                    "legacy_id",
-                    "notes",
-                    "last_modified_by__username",
-                    "last_modified_by__first_name",
-                    "last_modified_by__last_name",
-                    "last_modified_datetime",
-                )
-                .get(uuid=uuid)
+    def get_detail_queryset(self):
+        return (
+            HousingCompany.objects.select_related(
+                "postal_code",
+                "financing_method",
+                "developer",
+                "building_type",
+                "property_manager",
+                "property_manager__postal_code",
+                "last_modified_by",
             )
-        except ObjectDoesNotExist:
-            raise HousingCompanyNotFound()
-
-        serializer = HousingCompanyDetailSerializer(housing_company)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            .annotate(date=Min("real_estates__buildings__completion_date"))
+            .only(
+                "uuid",
+                "state",
+                "postal_code__value",
+                "postal_code__description",
+                "financing_method__value",
+                "financing_method__description",
+                "financing_method__legacy_code_number",
+                "developer__value",
+                "developer__description",
+                "developer__legacy_code_number",
+                "building_type__value",
+                "building_type__description",
+                "building_type__legacy_code_number",
+                "property_manager__name",
+                "property_manager__email",
+                "property_manager__street_address",
+                "property_manager__postal_code__value",
+                "property_manager__postal_code__description",
+                "display_name",
+                "street_address",
+                "business_id",
+                "official_name",
+                "acquisition_price",
+                "realized_acquisition_price",
+                "primary_loan",
+                "sales_price_catalogue_confirmation_date",
+                "notification_date",
+                "legacy_id",
+                "notes",
+                "last_modified_by__username",
+                "last_modified_by__first_name",
+                "last_modified_by__last_name",
+                "last_modified_datetime",
+            )
+        )
