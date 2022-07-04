b3f1be0d050be9ab7587830fd2a9be2220fea75d
Mats Eriksson <mats.eriksson@vincit.fi>
2022-07-04 04:47:37 +0300

    Adds rudimentary top-level routing and views

---
+++ frontend/src/pages/ApartmentsListing.tsx
@@ -0,0 +1,7 @@
+export default function Apartmentslisting() {
+    return (
+        <div className="ApartmentsListing">
+            <h1 className="main-heading">Asunnot</h1>
+        </div>
+    );
+}
---
+++ frontend/src/pages/Reports.tsx
@@ -0,0 +1,9 @@
+const Reports = () => {
+    return (
+        <div className="reports">
+            <h1 className="main-heading">Raportit</h1>
+        </div>
+    );
+};
+
+export default Reports;
