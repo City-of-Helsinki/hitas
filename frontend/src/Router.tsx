301baa79c15368cd8e7bc307466dc535ce753e73
Mats Eriksson <mats.eriksson@vincit.fi>
2022-07-05 23:55:53 +0300

    Refactored Router into its own component for clarity

---
+++ frontend/src/Router.tsx
@@ -0,0 +1,55 @@
+import { Route, Routes } from "react-router-dom";
+import App from "./App";
+import Companies from "./pages/Companies";
+import Apartments from "./pages/Apartments";
+import Reports from "./pages/Reports";
+import Documents from "./pages/Documents";
+import Codes from "./pages/Codes";
+import React from "react";
+
+export default function Router() {
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={<App />}
+            >
+                <Route
+                    path="yhtiot"
+                    element={<Companies />}
+                />
+                <Route
+                    path="asunnot"
+                    element={<Apartments />}
+                />
+                <Route
+                    path="raportit"
+                    element={<Reports />}
+                />
+                <Route
+                    path="dokumentit"
+                    element={<Documents />}
+                />
+                <Route
+                    path="koodisto"
+                    element={<Codes />}
+                />
+                <Route
+                    index
+                    element={<Companies />}
+                />
+                <Route
+                    path="*"
+                    element={
+                        <main style={{ padding: "1rem" }}>
+                            <h1>
+                                Päädyit tyhjälle sivulle. Ole hyvä ja
+                                tarkista osoite!
+                            </h1>
+                        </main>
+                    }
+                />
+            </Route>
+        </Routes>
+    );
+}
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import App from "./App";
-import Apartments from "./pages/Apartments";
-import Codes from "./pages/Codes";
-import Companies from "./pages/Companies";
-import Documents from "./pages/Documents";
-import Reports from "./pages/Reports";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./Router";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
@@ -15,47 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <BrowserRouter>
-        <Routes>
-            <Route
-                path="/"
-                element={<App />}
-            >
-                <Route
-                    path="yhtiot"
-                    element={<Companies />}
-                />
-                <Route
-                    path="asunnot"
-                    element={<Apartments />}
-                />
-                <Route
-                    path="raportit"
-                    element={<Reports />}
-                />
-                <Route
-                    path="dokumentit"
-                    element={<Documents />}
-                />
-                <Route
-                    path="koodisto"
-                    element={<Codes />}
-                />
-                <Route
-                    index
-                    element={<Companies />}
-                />
-                <Route
-                    path="*"
-                    element={
-                        <main style={{ padding: "1rem" }}>
-                            <h1>
-                                Päädyit tyhjälle sivulle. Ole hyvä ja
-                                tarkista osoite!
-                            </h1>
-                        </main>
-                    }
-                />
-            </Route>
-        </Routes>
+        <Router />
     </BrowserRouter>
 );
