ab6839ea756cd7248463c9059e214d2524107774
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-07-06 13:48:31 +0300

    Remove noqa: F401 comments

--- backend/hitas/admin/__init__.py
+++ backend/hitas/admin/__init__.py
@@ -1,3 +1,3 @@
-from hitas.admin.codes import CodeAdmin  # noqa: F401
-from hitas.admin.housing_company import HousingCompanyAdmin  # noqa: F401
-from hitas.admin.property_manager import PropertyManagerAdmin  # noqa: F401
+from hitas.admin.codes import CodeAdmin
+from hitas.admin.housing_company import HousingCompanyAdmin
+from hitas.admin.property_manager import PropertyManagerAdmin
