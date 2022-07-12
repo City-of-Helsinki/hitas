cf53ca51aa4e295389926d6438e2e8c4686c941c
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-07-12 04:13:07 +0300

    Add ViewSet for RealEstates

--- backend/hitas/urls.py
+++ backend/hitas/urls.py
@@ -1,12 +1,19 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework_nested.routers import NestedSimpleRouter
 
 from hitas import views
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"housing-companies", views.HousingCompanyViewSet, basename="housing-company")
 
+# Nested routers
+# e.g. /api/v1/housing-companies/{housing_company_id}/real-estates
+real_estate_router = NestedSimpleRouter(router, r"housing-companies", lookup="housing_company")
+real_estate_router.register(r"real-estates", views.RealEstateViewSet, basename="real-estate")
+
 app_name = "hitas"
 urlpatterns = [
     path("", include(router.urls)),
+    path("", include(real_estate_router.urls)),
 ]
--- backend/hitas/views/__init__.py
+++ backend/hitas/views/__init__.py
@@ -1,1 +1,2 @@
 from hitas.views.housing_company import HousingCompanyViewSet
+from hitas.views.real_estate import RealEstateViewSet
