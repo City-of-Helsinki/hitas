034d00d87c8e580109e9978d33e68cc98e7563e8
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-07-20 20:15:56 +0300

    Add tests for ApartmentType endpoints

--- backend/hitas/tests/factories/__init__.py
+++ backend/hitas/tests/factories/__init__.py
@@ -1,4 +1,10 @@
-from hitas.tests.factories.codes import BuildingTypeFactory, DeveloperFactory, FinancingMethodFactory, PostalCodeFactory
+from hitas.tests.factories.codes import (
+    ApartmentTypeFactory,
+    BuildingTypeFactory,
+    DeveloperFactory,
+    FinancingMethodFactory,
+    PostalCodeFactory,
+)
 from hitas.tests.factories.housing_company import BuildingFactory, HousingCompanyFactory, RealEstateFactory
 from hitas.tests.factories.property_manager import PropertyManagerFactory
 from hitas.tests.factories.user import UserFactory
