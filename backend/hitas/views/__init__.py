c5e99be027e47bfea4a7b593c5cb78ee745253dd
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-07-20 20:15:15 +0300

    Add ViewSet and Serializer for ApartmentType

--- backend/hitas/views/__init__.py
+++ backend/hitas/views/__init__.py
@@ -1,5 +1,11 @@
 from hitas.views.building import BuildingViewSet
-from hitas.views.codes import BuildingTypeViewSet, DeveloperViewSet, FinancingMethodViewSet, PostalCodeViewSet
+from hitas.views.codes import (
+    ApartmentTypeViewSet,
+    BuildingTypeViewSet,
+    DeveloperViewSet,
+    FinancingMethodViewSet,
+    PostalCodeViewSet,
+)
 from hitas.views.housing_company import HousingCompanyViewSet
 from hitas.views.property_manager import PropertyManagerViewSet
 from hitas.views.real_estate import RealEstateViewSet
