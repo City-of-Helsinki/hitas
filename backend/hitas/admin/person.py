65692786821dd83e7f5934c0018b069344fb6365
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-07-22 16:04:51 +0300

    Add initial Person and Owner models

---
+++ backend/hitas/admin/person.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+from hitas.models import Person
+from hitas.models.owner import Owner
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = [
+        "last_name",
+        "first_name",
+        "social_security_number",
+    ]
+    readonly_fields = ("uuid",)
+
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = [
+        "apartment",
+        "person",
+        "ownership_percentage",
+        "ownership_start_date",
+        "ownership_end_date",
+    ]
+    readonly_fields = ("uuid",)
