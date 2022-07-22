d8a2d3e47625d75285f990a0bc924339e98a2eb4
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-07-22 16:35:01 +0300

    Add initial REST API endpoints for Apartment, Person, Owner

---
+++ backend/hitas/views/person.py
@@ -0,0 +1,26 @@
+from hitas.models import Person
+from hitas.views.utils import AddressSerializer, HitasModelSerializer, HitasModelViewSet
+
+
+class PersonSerializer(HitasModelSerializer):
+    address = AddressSerializer(source="*")
+
+    class Meta:
+        model = Person
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "social_security_number",
+            "email",
+            "address",
+            "postal_code",
+        ]
+
+
+class PersonViewSet(HitasModelViewSet):
+    serializer_class = PersonSerializer
+    model_class = Person
+
+    def get_queryset(self):
+        return Person.objects.select_related("postal_code")
