be8faf24ec55e371e4c9a6f013731f70bc50a69a
Tomi Oikarinen <tomi.oikarinen@vincit.fi>
2022-08-16 10:02:17 +0300

    backend: added custom healthcheck
     - default `health_chech.db` does a INSERT + DELETE operation on
       healthcheck. that's a bit excessive. create a custom one
       that only does `SELECT 1`
     - locally this is about 8-9x faster than the standard module

--- backend/hitas/apps.py
+++ backend/hitas/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from health_check.plugins import plugin_dir
+
+from hitas.healthcheck import HitasDatabaseHealthCheck
 
 
 class HitasConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "hitas"
+
+    def ready(self):
+        plugin_dir.register(HitasDatabaseHealthCheck)
