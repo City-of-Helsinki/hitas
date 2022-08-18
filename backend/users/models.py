f74760a1abcde087cb8138b5bccf1a4831b1d34e
Tomi Oikarinen <tomi.oikarinen@vincit.fi>
2022-08-18 20:48:41 +0300

    backend: don't try to show email for users in __str__
     - instead of "Jokunen Jaska ()" show "Jaska Jokunen" as email from
       migrated users will be an empty string

--- backend/users/models.py
+++ backend/users/models.py
@@ -8,6 +8,9 @@ from helusers.models import AbstractUser
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__} ({self.pk}): {self.get_username()}>"
 
