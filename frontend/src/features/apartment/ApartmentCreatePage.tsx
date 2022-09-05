7cf476d4f68a97856aabb0622582b93081bf17f9
Mats Eriksson <mats.eriksson@vincit.fi>
2022-09-05 15:31:45 +0300

    Adds apartment type query and selection to apartment creation

--- frontend/src/features/apartment/ApartmentCreatePage.tsx
+++ frontend/src/features/apartment/ApartmentCreatePage.tsx
@@ -1,23 +1,30 @@
 import React from "react";
 
-import {Button, IconSaveDisketteFill} from "hds-react";
+import {Button, Fieldset, IconSaveDisketteFill} from "hds-react";
 import {DraftFunction, useImmer} from "use-immer";
 
-import {useCreateApartmentMutation} from "../../app/services";
+import {useCreateApartmentMutation, useGetApartmentTypesQuery} from "../../app/services";
 import {FormInputField} from "../../common/components";
-import {IApartmentWritable} from "../../common/models";
+import {IApartmentWritable, ICode} from "../../common/models";
 
-function SetApartmentFields({
+function ApartmentCreateFields({
     formData,
     setFormData,
     error,
 }: {
     formData: IApartmentWritable;
     setFormData: (arg: DraftFunction<IApartmentWritable> | IApartmentWritable) => void;
-    error: any;
+    error: unknown;
 }) {
     return (
-        <>
+        <Fieldset
+            heading="Asunnon tiedot"
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                gridGap: "1em",
+            }}
+        >
             <FormInputField
                 label="Katuosoite"
                 fieldPath="address.street_address"
@@ -27,6 +34,15 @@ function SetApartmentFields({
                 error={error}
             />
             <FormInputField
+                label="Rappu"
+                fieldPath="stair"
+                required
+                formData={formData}
+                setFormData={setFormData}
+                error={error}
+            />
+            <FormInputField
+                inputType="number"
                 label="Asunnon numero"
                 fieldPath="apartment_number"
                 required
@@ -34,19 +50,45 @@ function SetApartmentFields({
                 setFormData={setFormData}
                 error={error}
             />
-        </>
-    );
-}
-
-function SaveButton(props: {onClick: () => void}) {
-    return (
-        <Button
-            iconLeft={<IconSaveDisketteFill />}
-            onClick={props.onClick}
-            theme={"black"}
-        >
-            Tallenna
-        </Button>
+            <FormInputField
+                inputType="number"
+                label="Pinta-ala"
+                fieldPath="surface_area"
+                required
+                formData={formData}
+                setFormData={setFormData}
+                error={error}
+            />
+            <FormInputField
+                label="Asunto-osakeyhtiö"
+                fieldPath="housing_company.name"
+                required
+                formData={formData}
+                setFormData={setFormData}
+                error={error}
+            />
+            <FormInputField
+                inputType="relatedModel"
+                label="Asuntotyyppi"
+                fieldPath="apartment_type.id"
+                queryFunction={useGetApartmentTypesQuery}
+                relatedModelSearchField="value"
+                getRelatedModelLabel={(obj: ICode) => obj.value}
+                required
+                formData={formData}
+                setFormData={setFormData}
+                error={error}
+            />
+            <FormInputField
+                inputType="date"
+                label="Valmistumispäivä"
+                fieldPath="completion_date"
+                required
+                formData={formData}
+                setFormData={setFormData}
+                error={error}
+            />
+        </Fieldset>
     );
 }
 
@@ -59,21 +101,29 @@ const ApartmentCreatePage = () => {
         housing_company: {name: ""},
         surface_area: 0,
         apartment_number: 0,
-        completion_date: new Date(),
+        apartment_type: {id: ""},
+        apartment_building: "",
+        ownerships: [""],
     });
-    const [setApartment, {error}] = useCreateApartmentMutation();
+    const [createApartment, {error}] = useCreateApartmentMutation();
     const handleSaveButtonClicked = () => {
-        setApartment(formData);
+        createApartment(formData);
     };
     return (
         <div className="view--set-apartment">
             <h1 className="main-heading">Uusi asunto</h1>
-            <SetApartmentFields
+            <ApartmentCreateFields
                 formData={formData}
                 setFormData={setFormData}
                 error={error}
             />
-            <SaveButton onClick={handleSaveButtonClicked} />
+            <Button
+                iconLeft={<IconSaveDisketteFill />}
+                onClick={handleSaveButtonClicked}
+                theme={"black"}
+            >
+                Tallenna
+            </Button>
         </div>
     );
 };
