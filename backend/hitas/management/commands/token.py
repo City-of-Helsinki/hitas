c3ac20577834cad14c7b13923974385b39470daa
Tomi Oikarinen <tomi.oikarinen@vincit.fi>
2022-09-09 11:52:39 +0300

    backend: token management command: add support for revoking the token
     - `--key` renamed to `--set`
     - `--revoke` added

--- backend/hitas/management/commands/token.py
+++ backend/hitas/management/commands/token.py
@@ -8,11 +8,13 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument("--username", required=True, help="Username")
-        parser.add_argument("--key", help="Set key to a given value")
+        parser.add_argument("--set", help="Set user's token to a given value")
+        parser.add_argument("--revoke", action="store_true", help="Revoke the current token from the given user")
 
     def handle(self, *args, **options) -> None:
         username = options["username"]
-        key = options["key"]
+        key = options["set"]
+        revoke = options["revoke"]
 
         try:
             user = get_user_model().objects.get(username=username)
@@ -21,6 +23,10 @@ class Command(BaseCommand):
             return
 
         Token.objects.filter(user=user).delete()
+        if revoke:
+            print(f"Token revoked for user '{user.username}'.")
+            return
+
         token = Token.objects.create(user=user, key=key)
         if key:
             print(f"Token set for user '{user.username}'.")
