5ad688b53c9f06872d80a6d813f4fe9a5b4f1b17
Tomi Oikarinen <tomi.oikarinen@vincit.fi>
2022-09-14 12:07:53 +0300

    backend: update openapi-core

--- backend/hitas/tests/apis/helpers.py
+++ backend/hitas/tests/apis/helpers.py
@@ -1,29 +1,16 @@
-import openapi_core
+from typing import Optional
+
 import yaml
 from django.conf import settings
+from openapi_core import Spec
 from openapi_core.contrib.django import DjangoOpenAPIRequest, DjangoOpenAPIResponse
-from openapi_core.validation.request.datatypes import OpenAPIRequest
-from openapi_core.validation.response.validators import ResponseValidator
+from openapi_core.validation.response import openapi_response_validator
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.test import APIClient
 
 with open(f"{settings.BASE_DIR}/openapi.yaml", "r") as spec_file:
-    _openapi_spec = openapi_core.create_spec(yaml.safe_load(spec_file))
-
-
-from django.core.handlers.wsgi import WSGIRequest
-
-
-class WSGIRequestWorkaround:
-    def __init__(self, r: WSGIRequest):
-        self.r = r
-
-    def __getattr__(self, item):
-        if item == "body":
-            return self.r.META["wsgi.input"].read()
-
-        return getattr(self.r, item)
+    _openapi_spec = Spec.create(yaml.safe_load(spec_file))
 
 
 def validate_openapi(response: Response) -> None:
@@ -33,42 +20,44 @@ def validate_openapi(response: Response) -> None:
     if response.status_code == status.HTTP_204_NO_CONTENT:
         response["Content-Type"] = None
 
-    result = ResponseValidator(_openapi_spec).validate(
-        _openapi_url_pattern_workaround(DjangoOpenAPIRequest(WSGIRequestWorkaround(response.wsgi_request))),
+    result = openapi_response_validator.validate(
+        _openapi_spec,
+        OpenAPIUrlPatternWorkaround(response.wsgi_request),
         DjangoOpenAPIResponse(response),
     )
 
     result.raise_for_errors()
 
 
-def _openapi_url_pattern_workaround(original: OpenAPIRequest):
-    full_url_pattern = original.full_url_pattern
-    # For list view, DefaultRouter creates routes likes this:
-    # /api/v1/housing-companies$
-    # openapi-core does not understand the last $ when trying to look up the correct OpenAPI definition. let's remove it
-    if full_url_pattern[-1:] == "$":
-        full_url_pattern = full_url_pattern[:-1]
+class OpenAPIUrlPatternWorkaround(DjangoOpenAPIRequest):
+    @property
+    def path_pattern(self) -> Optional[str]:
+        if self.request.resolver_match is None:
+            return None
 
-    # For detail view, DefaultRouter creates routes likes this:
-    # - /api/v1/housing-companies/(?P<housing_company_id>[^/.]+)$
-    # openapi-core tries to simplify the URL so it can look the correct OpenAPI definition but fails and ends up with:
-    # - http://testserver/api/v1/housing-companies/{housing_company_id}/.]+)$
-    # - http://testserver/api/v1/housing-companies/{housing_company_id}/.]+)/real-estates$
-    # Let's remove the /.]+) from the url pattern if it's there ($ was removed in the previous step)
-    if full_url_pattern.find("/.]+)"):
-        full_url_pattern = full_url_pattern.replace("/.]+)", "")
+        route = self.request.resolver_match.route
 
-    return OpenAPIRequest(
-        full_url_pattern=full_url_pattern,
-        method=original.method,
-        parameters=original.parameters,
-        body=original.body,
-        mimetype=original.mimetype,
-    )
+        # For detail view, DefaultRouter creates routes likes this:
+        # - /api/v1/housing-companies/(?P<housing_company_id>[^/.]+)$
+        # openapi-core tries to simplify the URL so it can look the
+        # correct OpenAPI definition but fails and ends up with:
+        # - http://testserver/api/v1/housing-companies/{housing_company_id}/.]+)$
+        # - http://testserver/api/v1/housing-companies/{housing_company_id}/.]+)/real-estates$
+        # Let's remove the /.]+) from the url pattern if it's there ($ was removed in the previous step)
+        if route.find("/.]+)"):
+            route = route.replace("/.]+)", "")
+
+        route = self.path_regex.sub(r"{\1}", route)
+        # Delete start and end marker to allow concatenation.
+        if route[:1] == "^":
+            route = route[1:]
+        if route[-1:] == "$":
+            route = route[:-1]
+        return "/" + route
 
 
 class HitasAPIClient(APIClient):
-    def generic(self, method, path, data="", content_type="json", secure=False, **kwargs):
-        response = super().generic(method, path, data, content_type, secure=False, **kwargs)
+    def generic(self, method, path, data="", content_type="json", secure=False, **kwargs) -> Response:
+        response: Response = super().generic(method, path, data, content_type, secure=False, **kwargs)
         validate_openapi(response)
         return response
