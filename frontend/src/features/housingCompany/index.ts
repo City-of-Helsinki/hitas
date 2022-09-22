abbe59c06adcce9c3e5838065d0fcb0b981c2572
Mats Eriksson <mats.eriksson@vincit.fi>
2022-09-22 18:25:16 +0300

    Fixes the creation pages to be imported correctly via an index.js, as is the norm

--- frontend/src/features/housingCompany/index.ts
+++ frontend/src/features/housingCompany/index.ts
@@ -1,5 +1,13 @@
+import BuildingsCreatePage from "./BuildingsCreatePage";
 import HousingCompanyCreatePage from "./HousingCompanyCreatePage";
 import HousingCompanyDetailsPage from "./HousingCompanyDetailsPage";
 import HousingCompanyListPage from "./HousingCompanyListPage";
+import RealEstatesCreatePage from "./RealEstatesCreatePage";
 
-export {HousingCompanyCreatePage, HousingCompanyDetailsPage, HousingCompanyListPage};
+export {
+    HousingCompanyCreatePage,
+    HousingCompanyDetailsPage,
+    HousingCompanyListPage,
+    BuildingsCreatePage,
+    RealEstatesCreatePage,
+};
