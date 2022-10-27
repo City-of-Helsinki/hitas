dcf98c0a09fe70b031a491e7afa21b79fe5e2ce5
Tomi Oikarinen <tomi.oikarinen@vincit.fi>
2022-10-27 11:07:09 +0300

    backend: hitasmigration: fix type hint

--- backend/hitas/oracle_migration/runner.py
+++ backend/hitas/oracle_migration/runner.py
@@ -42,6 +42,7 @@ from hitas.models import (
     SurfaceAreaPriceCeiling,
 )
 from hitas.models.apartment import DepreciationPercentage
+from hitas.models.indices import AbstractIndex
 from hitas.oracle_migration.cost_areas import hitas_cost_area, init_cost_areas
 from hitas.oracle_migration.globals import anonymize_data, faker, should_anonymize
 from hitas.oracle_migration.oracle_schema import (
@@ -102,7 +103,7 @@ class ConvertedData:
 BULK_INSERT_THRESHOLD = 1000
 
 
-def create_indices(codes: List[LegacyRow], model_class: type[AbstractCode]) -> None:
+def create_indices(codes: List[LegacyRow], model_class: type[AbstractIndex]) -> None:
     for code in codes:
         index = model_class()
 
