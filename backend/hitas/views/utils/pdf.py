9a9d19e03c8057af01d1bc46ca9c5f1b5bc6de14
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-11-17 18:54:02 +0200

    backend: Add creation date to PDFs
    refs. HT-332

--- backend/hitas/views/utils/pdf.py
+++ backend/hitas/views/utils/pdf.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from io import BytesIO
 from typing import Any
 
 from django.http import HttpResponse
 from django.template.loader import get_template
+from django.utils import timezone
 from rest_framework import exceptions
 from xhtml2pdf import pisa
 
@@ -19,7 +21,7 @@ def render_to_pdf(template: str, context: dict[str, Any]) -> bytes:
 
 def get_pdf_response(filename: str, template: str, context: dict[str, Any]) -> HttpResponse:
     context.setdefault("title", filename)
-    pdf = render_to_pdf(template, context)
+    pdf = render_to_pdf(template, {**context, "date_today": datetime.strftime(timezone.now().today(), "%d.%m.%Y")})
     response = HttpResponse(pdf, content_type="application/pdf")
     response.headers["Content-Disposition"] = f"attachment; filename={filename}"
     return response
