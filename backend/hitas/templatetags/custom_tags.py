441ae46f18db847422149e0c0c233daeeed0152d
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-11-23 01:43:09 +0200

    backend: Initial version of Maximum Price Calculation PDF using indices
    (Market & Construction price indices)

--- backend/hitas/templatetags/custom_tags.py
+++ backend/hitas/templatetags/custom_tags.py
@@ -1,16 +1,26 @@
 from datetime import datetime
+from typing import Union
 
 import django_jinja.library
 from django.contrib.humanize.templatetags.humanize import intcomma as humanize_intcomma
+from num2words import num2words
 
 
 @django_jinja.library.filter
-def format_date(value: str) -> str:
+def format_date(value: Union[str, datetime.date]) -> str:
+    if type(value) == str:
+        value = datetime.strptime(value, "%Y-%m-%d")
+
     if value:
-        return datetime.strftime(datetime.strptime(value, "%Y-%m-%d"), "%d.%m.%Y")
+        return datetime.strftime(value, "%d.%m.%Y")
     return "-"
 
 
 @django_jinja.library.filter
 def intcomma(value) -> str:
     return humanize_intcomma(value)
+
+
+@django_jinja.library.filter
+def wordify(value: int) -> str:
+    return num2words(value, lang="fi")
