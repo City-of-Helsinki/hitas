41d15ca8448aaafbaa9cde5261c3d80b3ee70b7e
Tomi Oikarinen <tomi.oikarinen@vincit.fi>
2022-12-07 13:16:12 +0200

    backend: improve months_between_dates calculation
     - take days into account, don't just assume months
     - added tests

--- backend/hitas/calculations/helpers.py
+++ backend/hitas/calculations/helpers.py
@@ -2,6 +2,8 @@ import datetime
 from decimal import ROUND_HALF_UP, Decimal
 from typing import Optional
 
+from dateutil import relativedelta
+
 
 def roundup(v: Decimal) -> Optional[Decimal]:
     if v is None:
@@ -27,4 +29,10 @@ class NoneSum:
 
 
 def months_between_dates(first: datetime.date, second: datetime.date) -> int:
-    return (second.year - first.year) * 12 + (second.month - first.month)
+    second_day_is_last_day_of_month = (second + relativedelta.relativedelta(days=1)).month != second.month
+
+    return (
+        (second.year - first.year) * 12
+        + (second.month - first.month)
+        - (1 if second.day < first.day and not second_day_is_last_day_of_month else 0)
+    )
--- backend/hitas/tests/test_max_price.py
+++ backend/hitas/tests/test_max_price.py
@@ -2,6 +2,7 @@ import datetime
 
 import pytest
 
+from hitas.calculations.helpers import months_between_dates
 from hitas.calculations.max_prices.rules import surface_area_price_ceiling_validity
 
 
@@ -65,3 +66,38 @@ from hitas.calculations.max_prices.rules import surface_area_price_ceiling_valid
 )
 def test__surface_area_price_ceiling_validity(date: datetime.date, expected: datetime.date):
     assert surface_area_price_ceiling_validity(date) == expected
+
+
+@pytest.mark.parametrize(
+    "first, second, expected",
+    [
+        (datetime.date(2022, 1, 1), datetime.date(2022, 1, 1), 0),
+        (datetime.date(2022, 1, 1), datetime.date(2022, 1, 2), 0),
+        (datetime.date(2022, 1, 1), datetime.date(2022, 1, 31), 0),
+        (datetime.date(2022, 1, 1), datetime.date(2022, 2, 1), 1),
+        (datetime.date(2022, 1, 1), datetime.date(2022, 2, 2), 1),
+        (datetime.date(2022, 1, 1), datetime.date(2022, 2, 28), 1),
+        (datetime.date(2022, 1, 1), datetime.date(2022, 3, 1), 2),
+        (datetime.date(2022, 1, 1), datetime.date(2023, 1, 1), 12),
+        (datetime.date(2022, 1, 1), datetime.date(2023, 1, 2), 12),
+        (datetime.date(2022, 1, 1), datetime.date(2023, 1, 31), 12),
+        (datetime.date(2022, 1, 1), datetime.date(2023, 2, 1), 13),
+        (datetime.date(2022, 1, 15), datetime.date(2022, 1, 1), -1),
+        (datetime.date(2022, 1, 15), datetime.date(2022, 1, 14), -1),
+        (datetime.date(2022, 1, 15), datetime.date(2022, 1, 15), 0),
+        (datetime.date(2022, 1, 15), datetime.date(2022, 1, 31), 0),
+        (datetime.date(2022, 1, 15), datetime.date(2022, 2, 1), 0),
+        (datetime.date(2022, 1, 15), datetime.date(2022, 2, 14), 0),
+        (datetime.date(2022, 1, 15), datetime.date(2022, 2, 15), 1),
+        (datetime.date(2022, 1, 31), datetime.date(2022, 2, 1), 0),
+        (datetime.date(2022, 1, 31), datetime.date(2022, 2, 15), 0),
+        (datetime.date(2022, 1, 31), datetime.date(2022, 2, 27), 0),
+        (datetime.date(2022, 1, 31), datetime.date(2022, 2, 28), 1),
+        (datetime.date(2022, 1, 31), datetime.date(2022, 3, 30), 1),
+        (datetime.date(2022, 1, 31), datetime.date(2022, 3, 31), 2),
+        (datetime.date(2022, 1, 31), datetime.date(2022, 4, 29), 2),
+        (datetime.date(2022, 1, 31), datetime.date(2022, 4, 30), 3),
+    ],
+)
+def test__months_between_dates(first: datetime.date, second: datetime.date, expected: int):
+    assert months_between_dates(first, second) == expected
