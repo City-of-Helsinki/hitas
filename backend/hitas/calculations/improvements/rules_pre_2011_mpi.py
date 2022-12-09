f0c7d382e88ba383f854de20b9e38841505ee9b9
Tomi Oikarinen <tomi.oikarinen@vincit.fi>
2022-12-09 15:42:32 +0200

    backend: small style improvements

--- backend/hitas/calculations/improvements/rules_pre_2011_mpi.py
+++ backend/hitas/calculations/improvements/rules_pre_2011_mpi.py
@@ -115,7 +115,7 @@ def calculate_single_apartment_improvement_pre_2011_market_price_index(
         depreciation_amount = value_without_excess
     else:
         # Calculate the depreciation amount
-        depreciation_amount = Decimal(value_without_excess) / (10 * 12) * depreciation_time_months
+        depreciation_amount = value_without_excess / (10 * 12) * depreciation_time_months
 
     depreciation_result = ApartmentImprovementCalculationResult.Depreciation(
         time=ApartmentImprovementCalculationResult.Depreciation.DepreciationTime.create(depreciation_time_months),
@@ -279,7 +279,7 @@ def calculate_single_housing_company_improvement_pre_2011_market_price_index(
             depreciation_amount = value_without_excess
         else:
             # Calculate the depreciation amount
-            depreciation_amount = Decimal(value_without_excess) / (15 * 12) * depreciation_time_months
+            depreciation_amount = value_without_excess / (15 * 12) * depreciation_time_months
 
         depreciation_result = ApartmentImprovementCalculationResult.Depreciation(
             time=ApartmentImprovementCalculationResult.Depreciation.DepreciationTime.create(depreciation_time_months),
