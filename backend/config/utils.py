20685518e08c1023ba05aa7a2c9e8e931419d672
Matti Lamppu <matti.lamppu@vincit.fi>
2023-01-19 10:44:06 +0200

    Fix bootloop due to missing pyproject.toml file
    Some mechanism for figuring out backend version should be made at some
    point, so that devops can figure out which version is causing errors
    in production.

--- backend/config/utils.py
+++ backend/config/utils.py
@@ -7,7 +7,10 @@ import tomli
 @cache
 def get_current_version() -> str:
     base_dir = Path(__file__).parent.parent
-    with open(base_dir / "pyproject.toml", mode="rb") as f:
-        data = tomli.load(f)
+    try:
+        with open(base_dir / "pyproject.toml", mode="rb") as f:
+            data = tomli.load(f)
+    except Exception:  # noqa
+        return "0"
 
     return data["tool"]["poetry"]["version"]
