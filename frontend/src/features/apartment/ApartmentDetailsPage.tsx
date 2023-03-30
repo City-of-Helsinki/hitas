c0bfbe85847aa9ce662e79eef4dd992546f36c51
Mats Eriksson <mats.eriksson@vincit.fi>
2023-03-30 23:15:28 +0300

    front: disables the CoS-management button if there are no ownersips

--- frontend/src/features/apartment/ApartmentDetailsPage.tsx
+++ frontend/src/features/apartment/ApartmentDetailsPage.tsx
@@ -95,6 +95,7 @@ const ApartmentConditionsOfSaleCard = ({apartment}: {apartment: IApartmentDetail
                     <Button
                         theme="black"
                         iconLeft={<IconLock />}
+                        disabled={!apartment.ownerships.length}
                     >
                         Muokkaa myyntiehtoja
                     </Button>
@@ -111,7 +112,7 @@ const ApartmentConditionsOfSaleCard = ({apartment}: {apartment: IApartmentDetail
                     ))}
                 </ul>
             ) : (
-                <span>Ei myyntiehtoja.</span>
+                <span className="no-conditions">Ei myyntiehtoja.</span>
             )}
         </Card>
     );
