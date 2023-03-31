4463106ef2b152a086c39656303933aff72a316f
Matti Lamppu <matti.lamppu@vincit.fi>
2023-03-31 09:33:55 +0300

    Make default regulation status REGULATED for test factory

--- backend/hitas/tests/factories/housing_company.py
+++ backend/hitas/tests/factories/housing_company.py
@@ -25,7 +25,7 @@ class HousingCompanyFactory(DjangoModelFactory):
     state = fuzzy.FuzzyChoice(state[0] for state in HousingCompanyState.choices())
     hitas_type = fuzzy.FuzzyChoice(state[0] for state in HitasType.choices())
     exclude_from_statistics = False
-    regulation_status = fuzzy.FuzzyChoice(state[0] for state in RegulationStatus.choices())
+    regulation_status = RegulationStatus.REGULATED
     business_id = factory.Faker("company_business_id")
     street_address = factory.Faker("street_address")
     postal_code = factory.SubFactory("hitas.tests.factories.HitasPostalCodeFactory")
