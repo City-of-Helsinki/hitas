601fcf2907bf5b1c0c9d065fa6ed54659af18d0c
Mats Eriksson <mats.eriksson@vincit.fi>
2023-05-22 10:38:39 +0300

    front: implements new design in postal code replacement view

--- frontend/src/features/functions/components/index.ts
+++ frontend/src/features/functions/components/index.ts
@@ -1,12 +1,20 @@
 import ComparisonErrorModal from "./ComparisonErrorModal";
 import ComparisonResultListItem from "./ComparisonResultListItem";
+<<<<<<< HEAD
+=======
+import ComparisonResultModal from "./ComparisonResultModal";
+>>>>>>> bd562f2 (front: implements new design in postal code replacement view)
 import ComparisonSkippedList from "./ComparisonSkippedList";
 import ComparisonSkippedListItem from "./ComparisonSkippedListItem";
 import LoadedThirtyYearComparison from "./LoadedThirtyYearResults";
 
 export {
     ComparisonResultListItem,
+<<<<<<< HEAD
     ComparisonErrorModal,
+=======
+    ComparisonResultModal,
+>>>>>>> bd562f2 (front: implements new design in postal code replacement view)
     ComparisonSkippedList,
     ComparisonSkippedListItem,
     LoadedThirtyYearComparison,
