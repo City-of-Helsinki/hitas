7d1f6dd11b3ff633865cb90e902a0382b22ed721
Mats Eriksson <mats.eriksson@vincit.fi>
2023-05-26 01:07:18 +0300

    front: adds confirm dialog to manual company release action

--- frontend/src/features/functions/components/ThirtyYearResultListItem.tsx
+++ frontend/src/features/functions/components/ThirtyYearResultListItem.tsx
@@ -1,35 +1,66 @@
-import {Button} from "hds-react";
+import {Button, IconDocument, IconLockOpen} from "hds-react";
 import {useState} from "react";
 import {Link} from "react-router-dom";
 import {downloadCompanyRegulationLetter} from "../../../app/services";
-import {formatDate} from "../../../common/utils";
+import ConfirmDialogModal from "../../../common/components/ConfirmDialogModal";
+import {formatDate, hdsToast} from "../../../common/utils";
 
-const ThirtyYearResultListItem = ({company}) => {
+const ThirtyYearResultListItem = ({company, category}) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const [isClicked, setIsClicked] = useState(company.letter_fetched);
     const handleClick = () => {
         downloadCompanyRegulationLetter(company);
         setIsClicked(true);
     };
+    const handleFree = () => {
+        console.log(`Action to free ${company.display_name} from regulation`);
+        hdsToast.success(`Yhtiön ${company.display_name} manuaalinen vapautus onnistui.`);
+        setIsModalOpen(false);
+    };
     return (
         <li className="results-list__item">
-            <Link to={`/housing-companies/${company.id}`}>
+            <Link
+                to={`/housing-companies/${company.id}`}
+                target="_blank"
+            >
                 <div className="name">{company.display_name}</div>
                 <div className="address">
                     {company.address.street_address}
                     <br />
                     {company.address.postal_code}, {company.address.city}
                 </div>
-                <div className="date">{formatDate(company.completion_date)}</div>
-                <div className="property-manager">{company.property_manager.email}</div>
             </Link>
-            <Button
-                theme="black"
-                onClick={handleClick}
-                variant={isClicked ? "secondary" : "primary"}
-                className="download-button"
-            >
-                Hae {isClicked ? "uudelleen" : "tiedote"}
-            </Button>
+            <div className="date">{formatDate(company.completion_date)}</div>
+            <div className="property-manager">{company.property_manager.email}</div>
+            <div className="buttons">
+                {category !== "freed" && (
+                    <Button
+                        theme="black"
+                        onClick={() => setIsModalOpen(true)}
+                        iconLeft={<IconLockOpen />}
+                    >
+                        Vapauta manuaalisesti
+                    </Button>
+                )}
+                <Button
+                    theme="black"
+                    onClick={handleClick}
+                    variant={isClicked ? "secondary" : "primary"}
+                    className="download-button"
+                    iconLeft={<IconDocument />}
+                >
+                    Hae {isClicked ? "uudelleen" : "tiedote"}
+                </Button>
+            </div>
+            <ConfirmDialogModal
+                modalHeader={`Vapauta ${company.display_name}?`}
+                modalText={`Olet manuaalisesti vapauttamassa yhtiötä (esim tontit-yksikön päätöksestä). Haluatko varmasti, että ${company.display_name} vapautetaan sääntelyn piiristä?`}
+                isVisible={isModalOpen}
+                setIsVisible={setIsModalOpen}
+                buttonText="Vapauta"
+                confirmAction={handleFree}
+                cancelAction={() => setIsModalOpen(false)}
+            />
         </li>
     );
 };
