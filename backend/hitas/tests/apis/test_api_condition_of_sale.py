2f1d508558ff61e2c59b6b1387a8f21bedfa4b5e
Matti Lamppu <matti.lamppu@vincit.fi>
2023-05-29 12:29:21 +0300

    Dont allow deleting automatically created conditions of sale

--- backend/hitas/tests/apis/test_api_condition_of_sale.py
+++ backend/hitas/tests/apis/test_api_condition_of_sale.py
@@ -1265,11 +1265,17 @@ def test__api__condition_of_sale__update__invalid(api_client: HitasAPIClient, in
 
 
 @pytest.mark.django_db
-def test__api__condition_of_sale__delete(api_client: HitasAPIClient, freezer, settings):
+def test__api__condition_of_sale__delete__different_owners(api_client: HitasAPIClient, freezer, settings):
     freezer.move_to("2023-01-01 00:00:00+00:00")
     old_time = timezone.now()
 
-    condition_of_sale: ConditionOfSale = ConditionOfSaleFactory.create()
+    owner_1: Owner = OwnerFactory.create()
+    owner_2: Owner = OwnerFactory.create()
+
+    condition_of_sale: ConditionOfSale = ConditionOfSaleFactory.create(
+        new_ownership__owner=owner_1,
+        old_ownership__owner=owner_2,
+    )
 
     url = reverse(
         "hitas:conditions-of-sale-detail",
@@ -1278,6 +1284,7 @@ def test__api__condition_of_sale__delete(api_client: HitasAPIClient, freezer, se
         },
     )
     response_1 = api_client.delete(url)
+    # Deletion is allowed since the old and new owners are not the same (created through a household)
     assert response_1.status_code == status.HTTP_204_NO_CONTENT, response_1.json()
 
     freezer.move_to(old_time + settings.SHOW_FULFILLED_CONDITIONS_OF_SALE_FOR_MONTHS)
@@ -1291,6 +1298,33 @@ def test__api__condition_of_sale__delete(api_client: HitasAPIClient, freezer, se
     # Can't find it over 3 months after
     response_2 = api_client.get(url)
     assert response_2.status_code == status.HTTP_404_NOT_FOUND, response_2.json()
+
+
+@pytest.mark.django_db
+def test__api__condition_of_sale__delete__same_owners(api_client: HitasAPIClient, settings):
+    owner: Owner = OwnerFactory.create()
+
+    condition_of_sale: ConditionOfSale = ConditionOfSaleFactory.create(
+        new_ownership__owner=owner,
+        old_ownership__owner=owner,
+    )
+
+    url = reverse(
+        "hitas:conditions-of-sale-detail",
+        kwargs={
+            "uuid": condition_of_sale.uuid.hex,
+        },
+    )
+    response = api_client.delete(url)
+    # Deletion is not allowed since the old and new owners are the same (created through a sale)
+    assert response.status_code == status.HTTP_409_CONFLICT, response.json()
+
+    assert response.json() == {
+        "error": "invalid",
+        "message": "Cannot delete condition of sale between the same owner.",
+        "reason": "Conflict",
+        "status": 409,
+    }
 
 
 @pytest.mark.django_db
