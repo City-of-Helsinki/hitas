8cb672139b72c0d10ecceb85f9bd048c7102cdae
Eemeli Ranta <eemeli.ranta@vincit.fi>
2023-06-05 11:44:42 +0300

    frontend: Display a specific message if unconfirmed prices are missing

--- frontend/src/features/apartment/ApartmentNewSalePage/MaximumPriceCalculationMissing.tsx
+++ frontend/src/features/apartment/ApartmentNewSalePage/MaximumPriceCalculationMissing.tsx
@@ -12,11 +12,18 @@ const MaximumPriceCalculationMissing = () => {
     return (
         <div className="row row--prompt">
             <p>
-                Asunnosta ei ole vahvistettua enimmäishintalaskelmaa. Enimmäishintana käytetään asunnon
-                rajaneliöhinta-arviota <b>{formatMoney(unconfirmedPrices.surface_area_price_ceiling.value)}.</b>
-                <br />
-                Mikäli asunnon velaton kauppahinta ylittää rajaneliöhinta-arvion, tulee asunnolle luoda vahvistettu
-                enimmäishintalaskelma.
+                Asunnosta ei ole vahvistettua enimmäishintalaskelmaa.
+                {unconfirmedPrices.surface_area_price_ceiling.value ? (
+                    <>
+                        Enimmäishintana käytetään asunnon rajaneliöhinta-arviota
+                        <b> {formatMoney(unconfirmedPrices.surface_area_price_ceiling.value)}.</b>
+                        <br />
+                        Mikäli asunnon velaton kauppahinta ylittää rajaneliöhinta-arvion, tulee asunnolle luoda
+                        vahvistettu enimmäishintalaskelma.
+                    </>
+                ) : (
+                    <b> Asunnon rajaneliöhinta-arviota ei voitu laskea.</b>
+                )}
             </p>
         </div>
     );
