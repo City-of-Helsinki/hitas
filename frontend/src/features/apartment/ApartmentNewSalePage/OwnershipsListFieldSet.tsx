1420d8c6731162da1281a9a098aaefc2f15cc68c
Mats Eriksson <mats.eriksson@vincit.fi>
2023-06-05 14:12:39 +0300

    front: fixes error messages from rebase-mistake

--- frontend/src/features/apartment/ApartmentNewSalePage/OwnershipsListFieldSet.tsx
+++ frontend/src/features/apartment/ApartmentNewSalePage/OwnershipsListFieldSet.tsx
@@ -6,7 +6,7 @@ import {zodResolver} from "@hookform/resolvers/zod/dist/zod";
 import {useRef, useState} from "react";
 import {z} from "zod";
 import {useGetOwnersQuery, useSaveOwnerMutation} from "../../../app/services";
-import {Checkbox, NumberInput, RelatedModelInput} from "../../../common/components/form";
+import {NumberInput, RelatedModelInput} from "../../../common/components/form";
 import TextInput from "../../../common/components/form/TextInput";
 import SaveButton from "../../../common/components/SaveButton";
 import SimpleErrorMessage from "../../../common/components/SimpleErrorMessage";
@@ -96,16 +96,12 @@ const OwnerMutateForm = ({formObject, formObjectFieldPath, cancelButtonAction, c
                     label="Sähköpostiosoite"
                     formObject={ownerFormObject}
                 />
-                <Checkbox
-                    name="non_disclosure"
-                    label="Turvakielto"
-                    formObject={ownerFormObject}
-                />
             </form>
 
             <Dialog.ActionButtons>
                 <Button
                     theme="black"
+                    size="small"
                     iconLeft={<IconArrowLeft />}
                     onClick={cancelButtonAction}
                 >
@@ -181,18 +177,21 @@ const OwnershipsListFieldSet = () => {
                                     onClick={() => remove(index)}
                                 />
                             </div>
-                            {!formErrors.success &&
-                                formErrors.error &&
-                                formErrors.error.issues.map((e) => (
-                                    <SimpleErrorMessage
-                                        key={`${e.code}-${e.message}`}
-                                        errorMessage={e.message}
-                                    />
-                                ))}
                         </li>
                     ))}
                 </>
             </ul>
+
+            <>
+                {!formErrors.success &&
+                    formErrors.error &&
+                    formErrors.error.issues.map((e) => (
+                        <SimpleErrorMessage
+                            key={`${e.code}-${e.message}`}
+                            errorMessage={e.message}
+                        />
+                    ))}
+            </>
 
             <div className="row row--buttons">
                 <Button
