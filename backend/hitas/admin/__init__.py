b2217b5b0f3b413561e5678a2551a191dbdce09b
Matti Lamppu <matti.lamppu@vincit.fi>
2023-06-12 13:34:54 +0300

    Add email templates to admin panel

--- backend/hitas/admin/__init__.py
+++ backend/hitas/admin/__init__.py
@@ -2,6 +2,7 @@ from hitas.admin.apartment import ApartmentAdmin
 from hitas.admin.apartment_sale import ApartmentSaleAdmin
 from hitas.admin.codes import CodeAdmin
 from hitas.admin.condition_of_sale import ConditionOfSaleAdmin
+from hitas.admin.email_template import EmailTemplateAdmin
 from hitas.admin.external_sales_data import ExternalSalesDataAdmin
 from hitas.admin.housing_company import HousingCompanyAdmin
 from hitas.admin.indices import IndexAdmin
