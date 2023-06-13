f34e1825aa1b23d0e22e04bc44ddb609a51ffd2b
Matti Lamppu <matti.lamppu@vincit.fi>
2023-06-13 14:53:07 +0300

    Add PDFBody to admin panel

--- backend/hitas/admin/__init__.py
+++ backend/hitas/admin/__init__.py
@@ -7,6 +7,7 @@ from hitas.admin.external_sales_data import ExternalSalesDataAdmin
 from hitas.admin.housing_company import HousingCompanyAdmin
 from hitas.admin.indices import IndexAdmin
 from hitas.admin.owner import OwnerAdmin, OwnershipAdmin
+from hitas.admin.pdf_body import PDFBodyAdmin
 from hitas.admin.postal_code import HitasPostalCodeAdmin
 from hitas.admin.property_manager import PropertyManagerAdmin
 from hitas.admin.thirty_year_regulation import ThirtyYearRegulationResultsAdmin
