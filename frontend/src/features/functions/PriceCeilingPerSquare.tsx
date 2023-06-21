a16b58c70c70c543d4b1a9cd5ed1ef4f10af2ac4
Mats Eriksson <mats.eriksson@vincit.fi>
2023-06-21 17:04:38 +0300

    front: fixes form for calculating price ceiling

--- frontend/src/features/functions/PriceCeilingPerSquare.tsx
+++ frontend/src/features/functions/PriceCeilingPerSquare.tsx
@@ -1,12 +1,13 @@
 import {Button} from "hds-react";
 import {useState} from "react";
-import {useForm} from "react-hook-form";
 
-import {hitasQuarters} from "../../common/schemas";
-
-import {useGetIndicesQuery} from "../../app/services";
+import {
+    useCalculatePriceCeilingMutation,
+    useGetIndicesQuery,
+    useGetPriceCeilingCalculationDataQuery,
+} from "../../app/services";
 import {FilterTextInputField, Heading, QueryStateHandler} from "../../common/components";
-import {Select} from "../../common/components/form";
+import {getHitasQuarter, hdsToast, today} from "../../common/utils";
 
 export const years = Array.from({length: 34}, (_, index) => {
     const year = 2023 - index;
@@ -17,7 +18,6 @@ export const years = Array.from({length: 34}, (_, index) => {
 });
 
 const LoadedPriceCeilingPerSquareResultsList = ({data}) => {
-    console.log(data.contents);
     return (
         <div className="results">
             <div className="list-headers">
@@ -27,7 +27,7 @@ const LoadedPriceCeilingPerSquareResultsList = ({data}) => {
             <ul className="results-list">
                 {data?.contents.map((item) => (
                     <ListItem
-                        key={`${item.year}-${item.quarter}`}
+                        key={item.month}
                         month={item.month}
                         year={item.year}
                         value={item.value}
@@ -38,69 +38,105 @@ const LoadedPriceCeilingPerSquareResultsList = ({data}) => {
     );
 };
 
-const CalculateButton = () => (
-    <Button
-        className="calculate-button"
-        theme="black"
-        variant="secondary"
-        onClick={() => console.log("Calculating")}
-    >
-        <span>Laske</span>
-    </Button>
-);
-
 const ListItem = ({month, year, value}) => {
-    console.log(month);
     return (
         <li className="results-list__item">
             <div className="period">
                 {month}
                 {years[new Date().getFullYear() - year]?.label}
             </div>
-            <div className="value">{value ?? <CalculateButton />}</div>
+            <div className="value">{value}</div>
         </li>
     );
 };
 
+const PriceCeilingCalculationResult = ({data, currentMonth}) => {
+    const {
+        data: calculationData,
+        error,
+        isLoading,
+    } = useGetPriceCeilingCalculationDataQuery({calculationMonth: currentMonth});
+    return (
+        <QueryStateHandler
+            data={calculationData}
+            error={error}
+            isLoading={isLoading}
+        >
+            <span>Tälle neljännekselle on laskettu rajaneliöhinta:</span>
+            <span>{data.contents[0].value}</span>
+        </QueryStateHandler>
+    );
+};
+const PriceCeilingCalculationSection = ({data, currentMonth}) => {
+    const [calculatePriceCeiling] = useCalculatePriceCeilingMutation();
+    const handleCalculateButton = () => {
+        calculatePriceCeiling({
+            data: {calculation_month: currentMonth},
+        })
+            .then((data) => {
+                console.log(data);
+                hdsToast.success("Rajahinnan laskenta onnistui");
+            })
+            .catch((e) => {
+                console.warn(e);
+                hdsToast.error("Rajahinnan laskenta epäonnistui");
+            });
+    };
+    const CalculateButton = () => (
+        <Button
+            className="calculate-button"
+            theme="black"
+            onClick={handleCalculateButton}
+        >
+            <span>Laske rajaneliöhinta</span>
+        </Button>
+    );
+    return (
+        <div className="price-ceiling-calculation">
+            {data.contents.some((item) => item.month === currentMonth) ? (
+                <>
+                    <span>Tälle neljännekselle on laskettu rajaneliöhinta:</span>
+                    <span>{data.contents[0].value}</span>
+                    <PriceCeilingCalculationResult
+                        data={data}
+                        currentMonth={currentMonth}
+                    />
+                    <CalculateButton />
+                </>
+            ) : (
+                <>
+                    <p>Tälle neljännekselle ({getHitasQuarter().label}) ei vielä ole laskettu rajaneliöhintaa</p>
+                    <CalculateButton />
+                </>
+            )}
+        </div>
+    );
+};
+
 const PriceCeilingPerSquare = () => {
-    const [filterParams, setFilterParams] = useState({year: "2023"});
+    // FIXME: use today().slice(0, 7) for the current Month, the +3 is a hack for testing
+    const currentMonth = new Date().getFullYear() + "-" + ("0" + (new Date().getMonth() + 1)).slice(-2);
+    const [filterParams, setFilterParams] = useState({year: new Date().getFullYear().toString()});
     const {data, error, isLoading} = useGetIndicesQuery({
         indexType: "surface-area-price-ceiling",
         params: {...filterParams, limit: 12, page: 1},
     });
-    const formObject = useForm({
-        defaultValues: {year: years[1], quarter: hitasQuarters[0]},
-        mode: "all",
-    });
+    console.log(data?.contents[0].month, currentMonth, today().slice(0, 7));
 
     return (
         <div className="view--functions__price-ceiling-per-square">
             <Heading type="body">Rajaneliöhinnan laskenta</Heading>
-            <form>
-                <div>
-                    <Select
-                        label="Vuosi"
-                        options={years}
-                        name="year"
-                        formObject={formObject}
-                        defaultValue={years[0]}
-                    />
-                    <Select
-                        label="Ajanjakso"
-                        options={hitasQuarters}
-                        name="period"
-                        formObject={formObject}
-                        defaultValue={hitasQuarters[0]}
-                    />
-                    <CalculateButton />
-                </div>
-            </form>
-            <Heading type="body">Edelliset rajaneliöhinnat</Heading>
             <QueryStateHandler
                 data={data}
                 error={error}
                 isLoading={isLoading}
+                attemptedAction="Haetaan rajaneliöhintalistausta"
             >
+                <PriceCeilingCalculationSection
+                    data={data}
+                    currentMonth={currentMonth}
+                />
+                <Heading type="sub">Edelliset rajaneliöhinnat</Heading>
                 <LoadedPriceCeilingPerSquareResultsList data={data} />
             </QueryStateHandler>
             <div className="year-filter">
