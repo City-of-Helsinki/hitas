efd891d0913bc40dc828215932faf3affd89ed52
Mats Eriksson <mats.eriksson@vincit.fi>
2023-06-29 02:39:39 +0300

    front: restyles reports page and adds sales report download form

--- frontend/src/features/reports/Reports.tsx
+++ frontend/src/features/reports/Reports.tsx
@@ -1,4 +1,4 @@
-import {Tabs} from "hds-react";
+import {Accordion} from "hds-react";
 import {Heading} from "../../common/components";
 import HousingCompanyReports from "./HousingCompanyReports";
 import SalesReports from "./SalesReports";
@@ -7,26 +7,32 @@ const Reports = () => {
     return (
         <div className="view--reports">
             <Heading>Raportit</Heading>
-            <Tabs>
-                <Tabs.TabList>
-                    <Tabs.Tab>Taloyhtiön raportit</Tabs.Tab>
-                    <Tabs.Tab>Toteutuneet kauppahinnat</Tabs.Tab>
-                    <Tabs.Tab>Rajahintalaskelmat</Tabs.Tab>
-                    <Tabs.Tab>Enimmäishintojen vertailu</Tabs.Tab>
-                </Tabs.TabList>
-                <Tabs.TabPanel>
-                    <HousingCompanyReports />
-                </Tabs.TabPanel>
-                <Tabs.TabPanel>
+            <div className="reports-card">
+                <Accordion
+                    closeButton={false}
+                    heading="Toteutuneet kauppahinnat"
+                >
                     <SalesReports />
-                </Tabs.TabPanel>
-                <Tabs.TabPanel>
+                </Accordion>
+                <Accordion
+                    closeButton={false}
+                    heading="Yhtiön ja asuntojen raportit"
+                >
+                    <HousingCompanyReports />
+                </Accordion>
+                <Accordion
+                    closeButton={false}
+                    heading="Rajahintalaskelmat"
+                >
                     <h2>Raportit 3</h2>
-                </Tabs.TabPanel>
-                <Tabs.TabPanel>
+                </Accordion>
+                <Accordion
+                    closeButton={false}
+                    heading="Enimmäishintojen vertailu"
+                >
                     <h2>Raportit 4</h2>
-                </Tabs.TabPanel>
-            </Tabs>
+                </Accordion>
+            </div>
         </div>
     );
 };
--- frontend/src/features/reports/SalesReports.tsx
+++ frontend/src/features/reports/SalesReports.tsx
@@ -1,5 +1,46 @@
+import {Button} from "hds-react";
+import {useForm} from "react-hook-form";
+import {DateInput} from "../../common/components/form";
+import {today} from "../../common/utils";
+
 const SalesReports = () => {
-    return <h2>Toteutuneet kaupat aikavälillä</h2>;
+    const salesReportForm = useForm({
+        defaultValues: {
+            startDate: null,
+            endDate: today(),
+        },
+        mode: "all",
+    });
+    const {handleSubmit, watch} = salesReportForm;
+
+    return (
+        <form
+            className="row"
+            onSubmit={handleSubmit((data) => console.log(data))}
+        >
+            <DateInput
+                name="startDate"
+                label="Alkaen pvm"
+                formObject={salesReportForm}
+                tooltipText="Ensimmäinen päivä, jolta raportti lasketaan."
+                required
+            />
+            <DateInput
+                name="endDate"
+                label="Päättyen pvm"
+                formObject={salesReportForm}
+                tooltipText="Viimeinen päivä, jolta raportti lasketaan."
+                required
+            />
+            <Button
+                theme="black"
+                type="submit"
+                disabled={!watch("startDate") || !watch("endDate")}
+            >
+                Lataa raportti
+            </Button>
+        </form>
+    );
 };
 
 export default SalesReports;
