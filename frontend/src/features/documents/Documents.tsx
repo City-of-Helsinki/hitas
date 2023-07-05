f7814080f9b6147fc7884915bfa79d8ade3afb3f
Mats Eriksson <mats.eriksson@vincit.fi>
2023-07-06 02:06:59 +0300

    front: adds PDF template editing
    - the saving api-call still needs to be fixed

--- frontend/src/features/documents/Documents.tsx
+++ frontend/src/features/documents/Documents.tsx
@@ -1,10 +1,23 @@
+import {Tab, TabList, TabPanel, Tabs} from "hds-react";
+import ManageEmailTemplates from "./ManageEmailTemplates";
 import ManagePDFTemplates from "./ManagePDFTemplates";
 
 const Documents = () => {
     return (
         <div className="view--documents">
             <h1 className="main-heading">Dokumentit</h1>
-            <ManagePDFTemplates />
+            <Tabs>
+                <TabList>
+                    <Tab>PDF pohjat</Tab>
+                    <Tab>Sähköpostipohjat</Tab>
+                </TabList>
+                <TabPanel>
+                    <ManagePDFTemplates />
+                </TabPanel>
+                <TabPanel>
+                    <ManageEmailTemplates />
+                </TabPanel>
+            </Tabs>
         </div>
     );
 };
---
+++ frontend/src/features/documents/ManageEmailTemplates.tsx
@@ -0,0 +1,9 @@
+const ManageEmailTemplates = () => {
+    return (
+        <div>
+            <h1>Manage Email Templates</h1>
+        </div>
+    );
+};
+
+export default ManageEmailTemplates;
