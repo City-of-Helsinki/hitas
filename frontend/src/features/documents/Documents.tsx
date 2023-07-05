f41e59e8ade5a322fa70a91170a280182a5c820e
Mats Eriksson <mats.eriksson@vincit.fi>
2023-07-06 02:31:35 +0300

    front: fixes headings and their styling, Dokumentit => Dokumenttipohjat

--- frontend/src/features/documents/Documents.tsx
+++ frontend/src/features/documents/Documents.tsx
@@ -1,20 +1,23 @@
 import {Tab, TabList, TabPanel, Tabs} from "hds-react";
+import {Heading} from "../../common/components";
 import ManageEmailTemplates from "./ManageEmailTemplates";
 import ManagePDFTemplates from "./ManagePDFTemplates";
 
 const Documents = () => {
     return (
         <div className="view--documents">
-            <h1 className="main-heading">Dokumentit</h1>
+            <Heading type="main">Dokumenttipohjat</Heading>
             <Tabs>
                 <TabList>
-                    <Tab>PDF pohjat</Tab>
+                    <Tab>PDF-pohjat</Tab>
                     <Tab>Sähköpostipohjat</Tab>
                 </TabList>
                 <TabPanel>
+                    <h2>Ladattavien PDF-tiedostojen leipätekstit</h2>
                     <ManagePDFTemplates />
                 </TabPanel>
                 <TabPanel>
+                    <h2>Sähköpostipohjat</h2>
                     <ManageEmailTemplates />
                 </TabPanel>
             </Tabs>
