c34dfcfd390483cbab6c276ab00d9c6c498c3647
Eemeli Ranta <eemeli.ranta@vincit.fi>
2023-07-13 18:41:42 +0300

    frontend: Hide improvements for apartments using new hitas rules
    - Still show the table in case the apartment has any improvements previously

--- frontend/src/features/apartment/ApartmentImprovementsPage.tsx
+++ frontend/src/features/apartment/ApartmentImprovementsPage.tsx
@@ -1,11 +1,29 @@
 import {useContext} from "react";
 
+import {NavigateBackButton} from "../../common/components";
 import {GenericImprovementsPage} from "../../common/components/improvements";
 import ApartmentViewContextProvider, {ApartmentViewContext} from "./components/ApartmentViewContextProvider";
 
 const LoadedApartmentImprovementsPage = () => {
     const {housingCompany, apartment} = useContext(ApartmentViewContext);
     if (!apartment) throw new Error("Apartment not found");
+
+    // New Hitas apartments don't have any improvements so display an 'error' message if user somehow ended up here
+    if (
+        housingCompany.new_hitas &&
+        !apartment.improvements.market_price_index.length &&
+        !apartment.improvements.construction_price_index.length
+    ) {
+        return (
+            <div>
+                <p>Uusi-Hitas asunnoilla ei voi olla parannuksia.</p>
+                <div className="row row--buttons">
+                    <NavigateBackButton />
+                    <div />
+                </div>
+            </div>
+        );
+    }
 
     return (
         <GenericImprovementsPage
