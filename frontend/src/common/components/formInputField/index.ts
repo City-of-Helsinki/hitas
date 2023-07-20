50f4ae4c7ab33a4484fc913c1663c62cce2d92da
Eemeli Ranta <eemeli.ranta@vincit.fi>
2023-07-20 20:11:39 +0300

    frontend: Remove unused `FormInputField` and `NewOwnerForm` components

--- frontend/src/common/components/formInputField/index.ts
+++
@@ -1,3 +0,0 @@
-import FormInputField from "./FormInputField";
-
-export default FormInputField;
--- frontend/src/common/components/index.ts
+++ frontend/src/common/components/index.ts
@@ -4,14 +4,12 @@ import DeleteButton from "./DeleteButton";
 import DetailField from "./DetailField";
 import Divider from "./Divider";
 import EditButton from "./EditButton";
-import FormInputField from "./formInputField/FormInputField";
 import GenericActionModal from "./GenericActionModal";
 import GenericImprovementsPage from "./GenericImprovementsPage";
 import Heading from "./Heading";
 import ImprovementsTable from "./ImprovementsTable";
 import ListPageNumbers from "./ListPageNumbers";
 import NavigateBackButton from "./NavigateBackButton";
-import NewOwnerForm from "./NewOwnerForm";
 import Notifications from "./Notifications";
 import OwnershipList from "./OwnershipList";
 import QueryStateError from "./QueryStateError";
@@ -27,14 +25,12 @@ export {
     DetailField,
     Divider,
     EditButton,
-    FormInputField,
     GenericActionModal,
     GenericImprovementsPage,
     Heading,
     ImprovementsTable,
     ListPageNumbers,
     NavigateBackButton,
-    NewOwnerForm,
     Notifications,
     OwnershipList,
     QueryStateError,
