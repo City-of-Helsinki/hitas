ddac6ca23373194e6d677140dfb5c0da9ec1d816
Eemeli Ranta <eemeli.ranta@vincit.fi>
2023-08-25 20:40:03 +0300

    frontend: Rename ThirtyYearResultsSection file and component

--- frontend/src/features/functions/ThirtyYearRegulationTabComponents/index.ts
+++ frontend/src/features/functions/ThirtyYearRegulationTabComponents/index.ts
@@ -3,5 +3,5 @@ export {default as ThirtyYearErrorModal} from "./ThirtyYearErrorModal";
 export {default as ThirtyYearErrorTest} from "./ThirtyYearErrorTest";
 export {default as ThirtyYearLoadedResults} from "./ThirtyYearLoadedResults";
 export {default as ThirtyYearResultListItem} from "./ThirtyYearResultListItem";
-export {default as ThirtyYearResults} from "./ThirtyYearResults";
 export {default as ThirtyYearSkippedList} from "./ThirtyYearSkippedList";
+export {default as ThirtyYearResultsSection} from "./ThirtyYearResultsSection";
