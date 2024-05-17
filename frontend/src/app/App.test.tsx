aa553ece2c71b9976f60fb88ccaf8ac13ee7843c
indigane <indigane@users.noreply.github.com>
2024-05-17 14:10:27 +0300

    frontend: Change service name from Asuntopalvelut to Asumisen palvelut

--- frontend/src/app/App.test.tsx
+++ frontend/src/app/App.test.tsx
@@ -4,12 +4,12 @@ import {Provider} from "react-redux";
 import App from "./App";
 import {store} from "./store";
 
-test("renders Asuntopalvelut heading", () => {
+test("renders Asumisen palvelut heading", () => {
     render(
         <Provider store={store}>
             <App />
         </Provider>
     );
-    const linkElement = screen.getAllByText(/Asuntopalvelut/i)[0];
+    const linkElement = screen.getAllByText(/Asumisen palvelut/i)[0];
     expect(linkElement).toBeInTheDocument();
 });
