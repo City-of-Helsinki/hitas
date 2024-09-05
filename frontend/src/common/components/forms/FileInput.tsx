9b053d7b6b95fa2b9e4633bae457488b44aa48a7
indigane <indigane@users.noreply.github.com>
2024-09-05 10:51:15 +0300

    Fix drag and drop native input required validation error

--- frontend/src/common/components/forms/FileInput.tsx
+++ frontend/src/common/components/forms/FileInput.tsx
@@ -1,6 +1,6 @@
 import {FileInput as HDSFileInput} from "hds-react";
 
-import React from "react";
+import React, {useRef} from "react";
 import {useFormContext} from "react-hook-form";
 import {FormInputProps} from "./";
 
@@ -10,6 +10,7 @@ interface FileInputProps extends FormInputProps {
 }
 const FileInput = ({name, label = "", required, onChange, ...rest}: FileInputProps): React.JSX.Element => {
     const formObject = useFormContext();
+    const containerRef = useRef(null);
 
     const {
         register,
@@ -17,15 +18,27 @@ const FileInput = ({name, label = "", required, onChange, ...rest}: FileInputPro
     } = formObject;
     register(name, {required: required});
 
-    const handleChange = (e) => {
-        // Set the value of the input to the file selected
-        formObject.setValue(name, e[0]);
+    const handleChange = (filesArray) => {
+        // File is a required field but the HDS file input clears the native input after selection so the form is always invalid.
+        // This is a hack to make sure the native input has a file if the user has selected one so that the form can be submitted.
+        const containerElement = containerRef.current as HTMLElement | null;
+        const fileInput = containerElement?.querySelector('input[type="file"]') as HTMLInputElement | null;
+        if (fileInput) {
+            const datatransfer = new DataTransfer();
+            datatransfer.items.add(filesArray[0]);
+            setTimeout(() => (fileInput.files = datatransfer.files), 10);
+        }
+        // Explicitly mark as dirty, since react-hook-form does not support File objects for dirty checking.
+        formObject.setValue(name, filesArray[0], {shouldDirty: true});
         // If there is an onChange handler, call it
-        onChange && onChange(e);
+        onChange && onChange(filesArray);
     };
 
     return (
-        <div className="input-field--file">
+        <div
+            ref={containerRef}
+            className="input-field--file"
+        >
             <HDSFileInput
                 id={name}
                 label={label}
