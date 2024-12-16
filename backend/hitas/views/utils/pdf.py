ec03695a192196cc0c00b6b42ebb8648672784bd
indigane <indigane@users.noreply.github.com>
2024-12-16 12:04:13 +0200

    Add Helsinki logo to the PDF templates

--- backend/hitas/views/utils/pdf.py
+++ backend/hitas/views/utils/pdf.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from io import BytesIO
 from typing import Any
 
+from django.conf import settings
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.utils import timezone
@@ -12,7 +13,14 @@ from xhtml2pdf import pisa
 def render_to_pdf(template: str, context: dict[str, Any]) -> bytes:
     """Render given template to a pdf"""
     result = BytesIO()
-    template = get_template(template).render(context)
+    template = get_template(template).render(
+        {
+            **context,
+            # Due to the limitations of the xhtml2pdf library a file system path
+            # is required instead of a Django static files url.
+            "logo_path": settings.BASE_DIR / "hitas/static/helsinki_kehystunnus_musta.png",
+        },
+    )
     pdf = pisa.pisaDocument(BytesIO(template.encode("UTF-8")), dest=result)
     if pdf.err:
         raise exceptions.APIException(pdf.err)
@@ -21,7 +29,13 @@ def render_to_pdf(template: str, context: dict[str, Any]) -> bytes:
 
 def get_pdf_response(filename: str, template: str, context: dict[str, Any]) -> HttpResponse:
     context.setdefault("title", filename)
-    pdf = render_to_pdf(template, {**context, "date_today": datetime.strftime(timezone.now().date(), "%d.%m.%Y")})
+    pdf = render_to_pdf(
+        template,
+        {
+            **context,
+            "date_today": datetime.strftime(timezone.now().date(), "%d.%m.%Y"),
+        },
+    )
     response = HttpResponse(pdf, content_type="application/pdf")
     response.headers["Content-Disposition"] = f"attachment; filename={filename}"
     return response
